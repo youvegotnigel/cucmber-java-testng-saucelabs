4f85cd4e99e7ba562f113fdd9adad3211e9b5e39
youvegotnigel <nigeldave19@gmail.com>
2021-05-16 00:48:19 +0530

    create page inventory test

---
+++ src/test/java/stepdefs/InventoryPageTests.java
@@ -0,0 +1,19 @@
+package stepdefs;
+
+import base.BaseClass;
+import cucumber.api.java.en.And;
+import org.testng.Assert;
+import pages.LoginPage;
+
+public class InventoryPageTests extends BaseClass {
+
+    //InventoryPage inventoryPage = new InventoryPage(driver);
+    LoginPage loginPage = new LoginPage(driver);
+
+    @And("System should display page header '(.+)'")
+    public void verify_inventory_page_landing(String text) {
+
+        Assert.assertEquals(loginPage.goToInventoryPage().getPageHeader(), text);
+    }
+
+}
