ab1941f56dcdbeb0d563343e65f8413cec945688
youvegotnigel <nigeldave19@gmail.com>
2021-05-16 00:45:49 +0530

    add new base class

---
+++ src/test/java/base/BaseClass.java
@@ -0,0 +1,130 @@
+package base;
+
+import com.google.common.io.Files;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import pages.LoginPage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class BaseClass{
+
+    public static WebDriver driver;
+    public static Properties config;
+    protected LoginPage loginPage;
+
+    public void LoadConfigProperty() throws IOException {
+        config = new Properties();
+        FileInputStream ip = new FileInputStream(
+                System.getProperty("user.dir") + "//src//test//resources//config//config.properties");
+        config.load(ip);
+    }
+
+    public void openBrowser() throws Exception {
+        // loads the config options
+        LoadConfigProperty();
+
+        if (config.getProperty("browserType").equals("firefox")) {
+
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+
+        }else if(config.getProperty("browserType").equals("edge")){
+
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+
+        }else if (config.getProperty("browserType").equals("chrome")) {
+
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setHeadless(false);
+            driver = new ChromeDriver(options);
+        }
+    }
+
+    public void maximizeWindow() {
+        driver.manage().window().maximize();
+        loginPage = new LoginPage(this.driver);
+    }
+
+    public void implicitWait(int time) {
+        driver.manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
+    }
+
+    public void explicitWait(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, 3000);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void pageLoad(int time) {
+        driver.manage().timeouts().pageLoadTimeout(time, TimeUnit.SECONDS);
+    }
+
+    public void deleteAllCookies() {
+        driver.manage().deleteAllCookies();
+    }
+
+    public void setEnv() throws Exception {
+        LoadConfigProperty();
+        String baseUrl = config.getProperty("siteUrl");
+        driver.get(baseUrl);
+    }
+
+    public static String currentDateTime() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Calendar cal = Calendar.getInstance();
+        String cal1 = dateFormat.format(cal.getTime());
+        return cal1;
+    }
+
+
+    public void takeScreenshot() throws IOException {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File targetFile = new File(System.getProperty("user.dir") + "//screenshots/screenshot.png");
+        targetFile.getParentFile().mkdir();
+        targetFile.createNewFile();
+        Files.copy(scrFile, targetFile);
+
+    }
+
+
+    public void takeScreenshot2(ITestResult result) throws IOException {
+        if (result.isSuccess()) {
+            File imageFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String failureImageFileName = result.getMethod().getMethodName()
+                    + new SimpleDateFormat("MM-dd-yyyy_HH-ss").format(new GregorianCalendar().getTime()) + ".png";
+            File failureImageFile = new File(System.getProperty("user.dir") + "//screenshots//" + failureImageFileName);
+            failureImageFile.getParentFile().mkdir();
+            failureImageFile.createNewFile();
+            Files.copy(imageFile, failureImageFile);
+        }
+
+    }
+
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    public WebDriver getDriver(){
+        return driver;
+    }
+}
