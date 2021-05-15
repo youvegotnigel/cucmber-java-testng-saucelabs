1bf1bf10931bd32274c11ddb4c1740e0e3ce5026
youvegotnigel <nigeldave19@gmail.com>
2021-05-16 00:44:53 +0530

    add test runner

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,52 @@
+import base.BaseClass;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import cucumber.api.testng.TestNGCucumberRunner;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import helpers.ReportHelper;
+import org.testng.annotations.*;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"stepdefs"},
+        tags = {"~@Ignore"},
+        format = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "rerun:target/cucumber-reports/rerun.txt"
+        })
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    private TestNGCucumberRunner testNGCucumberRunner;
+    BaseClass baseClass = new BaseClass();
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {
+        baseClass.openBrowser();
+        baseClass.maximizeWindow();
+        baseClass.implicitWait(30);
+        baseClass.deleteAllCookies();
+        baseClass.setEnv();
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    }
+
+    @DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideFeatures();
+    }
+
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception{
+        testNGCucumberRunner.finish();
+        ReportHelper.generateCucumberReport();
+        baseClass.tearDown();
+    }
+
+}
