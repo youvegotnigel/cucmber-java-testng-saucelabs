2c4d64b37d5deed4185b6ea816e0d9305a63233f
youvegotnigel <nigeldave19@gmail.com>
2021-05-23 12:49:13 +0530

    remove logs

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -26,47 +26,47 @@ public class LoginPage {
     public void setUsername(String username) {
         driver.findElement(usernameTextBox).clear();
         driver.findElement(usernameTextBox).sendKeys(username);
-        log.info("Element found for username: " + usernameTextBox);
-        log.debug("Input " + username + "for username");
+        //log.info("Element found for username: " + usernameTextBox);
+        //log.debug("Input " + username + "for username");
     }
 
     public void setPassword(String password) {
         driver.findElement(passwordTextBox).clear();
         driver.findElement(passwordTextBox).sendKeys(password);
-        log.info("Element found for password : " + passwordTextBox);
-        log.debug("Input " + password + "for password");
+        //log.info("Element found for password : " + passwordTextBox);
+        //log.debug("Input " + password + "for password");
     }
 
     public void clickLoginButton() {
         driver.findElement(loginButton).click();
-        log.info("Element found for login button : " + loginButton);
-        log.debug("Click on login button");
+        //log.info("Element found for login button : " + loginButton);
+        //log.debug("Click on login button");
     }
 
     public boolean logoIsDisplayed() {
 
-        log.info("Element found for logo : " + logo);
-        log.debug("Logo is displayed");
+        //log.info("Element found for logo : " + logo);
+        //log.debug("Logo is displayed");
         return driver.findElement(logo).isDisplayed();
     }
 
     public String getErrorMsg() {
 
-        log.info("Element found for error message : " + errorMsg);
-        log.debug("Error message is displayed");
+        //log.info("Element found for error message : " + errorMsg);
+        //log.debug("Error message is displayed");
         return driver.findElement(errorMsg).getText();
     }
 
     public String getPageTitle() {
 
-        log.debug("Found page title : " + driver.getTitle());
+        //log.debug("Found page title : " + driver.getTitle());
         return driver.getTitle();
     }
 
     //pass the driver for the next page
     public InventoryPage goToInventoryPage() {
 
-        log.debug("Passing driver to inventory page");
+        //log.debug("Passing driver to inventory page");
         return new InventoryPage(driver);
     }
 }
