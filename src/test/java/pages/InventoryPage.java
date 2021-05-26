ebb4154186dbb94b9a6a63211343851fd4bb07a9
youvegotnigel <nigeldave19@gmail.com>
2021-05-26 14:24:08 +0530

    Update InventoryPage.java

--- src/test/java/pages/InventoryPage.java
+++ src/test/java/pages/InventoryPage.java
@@ -13,6 +13,7 @@ public class InventoryPage {
     private By item_names = By.xpath("//div[@class='inventory_item_name']");
     private By item_prices = By.xpath("//div[@class='inventory_item_price']");
     private By item_images = By.xpath("//img[@class='inventory_item_img']");
+    private By item_description = By.xpath("//div[@class='inventory_item_desc']");
     private By filter_icon = By.xpath("//select[@class='product_sort_container']");
     private By shopping_cart_badge = By.xpath("//div[@id='shopping_cart_container']");
     private By cart_item_count = By.xpath("//span[@class='shopping_cart_badge']");
@@ -31,9 +32,12 @@ public class InventoryPage {
         return driver.findElements(item_prices);
     }
 
-    public String getItemImagesCount(){
-        int count =  driver.findElements(item_images).size();
-        return Integer.toString(count);
+    public int getItemImagesCount(){
+        return driver.findElements(item_images).size();
+    }
+
+    public int getItemDescriptionCount(){
+        return driver.findElements(item_description).size();
     }
 
     public WebElement getFilterIcon(){
