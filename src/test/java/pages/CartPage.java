50f77be23ab6383516e03d9f670524b7ec468abb
youvegotnigel <nigeldave19@gmail.com>
2021-05-26 20:30:52 +0530

    Create CartPage.java

---
+++ src/test/java/pages/CartPage.java
@@ -0,0 +1,37 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+
+    private WebDriver driver;
+    private By pageHeader = By.xpath("//span[@class='title']");
+    private By button_checkout = By.xpath("//button[@id='checkout']");
+
+    //Constructor
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Methods
+    public String getPageHeader(){
+        return driver.findElement(pageHeader).getText();
+    }
+
+    public boolean textDisplayed(String text){
+        String xpath = "//div[normalize-space()='"+ text +"']";
+        return driver.findElement(By.xpath(xpath)).isDisplayed();
+    }
+
+    public void clickOnCheckout(){
+        driver.findElement(button_checkout).click();
+    }
+
+    //pass the driver for the next page
+    public CheckoutPage goToCheckoutPage() {
+        //log.debug("Passing driver to inventory page");
+        return new CheckoutPage(driver);
+    }
+
+}
