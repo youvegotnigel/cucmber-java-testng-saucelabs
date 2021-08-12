dfe751a3c72a5088e75de5f18f5a34e1a2a1b111
youvegotnigel <nigeldave19@gmail.com>
2021-08-12 23:34:09 +0530

    add console logs to cucumber report and log files

--- src/test/java/com/youvegotnigel/automation/stepdefs/ServiceHooks.java
+++ src/test/java/com/youvegotnigel/automation/stepdefs/ServiceHooks.java
@@ -1,16 +1,24 @@
 package com.youvegotnigel.automation.stepdefs;
 
 import com.youvegotnigel.automation.base.TestBase;
-import io.cucumber.java.*;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
 
 public class ServiceHooks {
 
     TestBase testBase;
+    private DevTools devTools;
     public static final Logger log = LogManager.getLogger(ServiceHooks.class.getName());
 
     @Before
@@ -49,8 +57,28 @@ public class ServiceHooks {
         }
         if(scenario.isFailed()){
             log.error("✘ Failed scenario : " + scenario.getName());
+            analyzeLog(scenario);
+
         }
         testBase.tearDown();
+    }
+
+    public void analyzeLog(Scenario scenario){
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        LogEntries logEntries = testBase.eventFiringWebDriver.manage().logs().get(LogType.BROWSER);
+        for (LogEntry entry : logEntries) {
+            //System.out.println(new Date(entry.getTimestamp()) + " " + entry.getLevel() + " " + entry.getMessage());
+            log.debug("\n*****************CONSOLE LOGS START*****************\n");
+            log.debug(entry.getMessage());
+            log.debug("\n******************CONSOLE LOGS END******************\n");
+            scenario.attach(entry.getMessage(), "text/plain","CONSOLE LOGS");
+        }
     }
 
 //    @BeforeStep
