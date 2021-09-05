9543a377d0fb881440616dfa7c245b42880fa699
youvegotnigel <nigeldave19@gmail.com>
2021-09-05 12:35:54 +0530

    run tests in parallel

--- src/test/java/com/youvegotnigel/automation/runner/TestRunner.java
+++ src/test/java/com/youvegotnigel/automation/runner/TestRunner.java
@@ -1,18 +1,16 @@
 package com.youvegotnigel.automation.runner;
 
-import com.youvegotnigel.automation.utils.ReportHelper;
-import io.cucumber.testng.*;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.TestNGCucumberRunner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 
 @CucumberOptions(
         features = "src/test/resources/features/default",
         glue = {"com/youvegotnigel/automation/stepdefs"},
-        tags = "@regression and not @ignore",
+        tags = "@regression1 and not @ignore",
         dryRun = false,
         plugin = {
                 "pretty",
@@ -24,37 +22,38 @@ public class TestRunner extends AbstractTestNGCucumberTests{
 
     private TestNGCucumberRunner testNGCucumberRunner;
     public static final Logger log = LogManager.getLogger(TestRunner.class.getName());
+//
+//    @BeforeClass(alwaysRun = true)
+//    @Override
+//    public void setUpClass(){
+//        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+//    }
+//
+//    @Test(groups = "cucumber", description = "Runs Cucumber Scenarios", dataProvider = "scenarios")
+//    @Override
+//    public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
+//        // the 'featureWrapper' parameter solely exists to display the feature
+//        // file in a test report
+//        testNGCucumberRunner.runScenario(pickleWrapper.getPickle());
+//    }
 
-    @BeforeClass(alwaysRun = true)
     @Override
-    public void setUpClass(){
-        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-    }
-
-    @Test(groups = "cucumber", description = "Runs Cucumber Scenarios", dataProvider = "scenarios")
-    @Override
-    public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
-        // the 'featureWrapper' parameter solely exists to display the feature
-        // file in a test report
-        testNGCucumberRunner.runScenario(pickleWrapper.getPickle());
-    }
-
-    @DataProvider
-    @Override
+    @DataProvider(parallel = true)
     public Object[][] scenarios() {
-        return testNGCucumberRunner.provideScenarios();
+        //return testNGCucumberRunner.provideScenarios();
+        return super.scenarios();
     }
 
-    @AfterClass(alwaysRun = true)
-    @Override
-    public void tearDownClass() {
-
-        testNGCucumberRunner.finish();
-        try {
-            ReportHelper.generateCucumberReport();
-        }catch (Exception e){
-            log.warn("No feature files found");
-            log.error(e.getMessage());
-        }
-    }
+//    @AfterClass(alwaysRun = true)
+//    @Override
+//    public void tearDownClass() {
+//
+//        //testNGCucumberRunner.finish();
+//        try {
+//            //ReportHelper.generateCucumberReport();
+//        }catch (Exception e){
+//            log.warn("No feature files found");
+//            log.error(e.getMessage());
+//        }
+//    }
 }
--- src/test/resources/features/default/03_Cart.feature
+++ src/test/resources/features/default/03_Cart.feature
@@ -1,15 +1,21 @@
-@regression
+@regression1
 Feature: Add to Cart Functionality
 
-  Background: User navigates to Application URL
+#  Background: User navigates to Application URL
+#    Given I log in as standard user
+#    And I add all items to cart
+#    And I click on Shopping cart badge
+
+  Scenario: Verify page navigation to Cart
     Given I log in as standard user
     And I add all items to cart
     And I click on Shopping cart badge
-
-  Scenario: Verify page navigation to Cart
     Then  System should display page header 'YOUR CART'
 
   Scenario: Verify all items are displayed
+    Given I log in as standard user
+    And I add all items to cart
+    And I click on Shopping cart badge
     Then I should see the items in my cart as below:
       | item_name                         | item_price |
       | Sauce Labs Backpack               | $29.99     |
