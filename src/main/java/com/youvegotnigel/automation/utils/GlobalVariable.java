4c60ba1215e305eb28358ec0382c3ea160fc2a4c
nigelMulholland <nigeldave19@gmail.com>
2021-10-03 17:38:30 +0530

    Add Global Variable class

---
+++ src/main/java/com/youvegotnigel/automation/utils/GlobalVariable.java
@@ -0,0 +1,10 @@
+package com.youvegotnigel.automation.utils;
+
+/**
+ * This class is used to handle static and non-static global variables
+ */
+public class GlobalVariable {
+
+    public static final int WAIT_TIME = 10;
+
+}
